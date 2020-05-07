028840b61abf7c838c5f09c093ed460fb6b49178
Ryan Bruns <ryan.bruns7@gmail.com>
2020-05-06 20:14:41 -0500

    simple dashboard is now in place.

--- apps/mammoth/src/app/modules/features/dashboard/dashboard.component.ts
+++ apps/mammoth/src/app/modules/features/dashboard/dashboard.component.ts
@@ -1,5 +1,5 @@
 import { MediaMatcher } from '@angular/cdk/layout';
-import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -7,21 +7,10 @@ import { ActivatedRoute } from '@angular/router';
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent implements OnInit {
-  mobileQuery: MediaQueryList;
-
-  fillerNav = Array.from({ length: 50 }, (_, i) => `Nav Item ${i + 1}`);
-
-  fillerContent = Array.from(
-    { length: 50 },
-    () =>
-      `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-      labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-      laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-      voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-       cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`
-  );
-
+export class DashboardComponent implements OnDestroy {
+  public mobileQuery: MediaQueryList;
+  public fillerNav: string[] = [];
+  public fillerContent = [];
   private _mobileQueryListener: () => void;
 
   constructor(
@@ -33,11 +22,14 @@ export class DashboardComponent implements OnInit {
     this.mobileQuery = media.matchMedia('(max-width: 600px)');
     this._mobileQueryListener = () => changeDetectorRef.detectChanges();
     this.mobileQuery.addListener(this._mobileQueryListener);
+    this.fillerNav = Array.from({ length: 50 }, (_, i) => `Nav Item ${i + 1}`);
+    this.fillerContent = Array.from(
+      { length: 50 },
+      () => `Bunch of filler content`
+    );
   }
 
-  ngOnDestroy(): void {
+  public ngOnDestroy(): void {
     this.mobileQuery.removeListener(this._mobileQueryListener);
   }
-
-  ngOnInit(): void {}
 }
