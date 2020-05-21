6c7ec8e63f7fd27127466fea4be3298954367b0d
Ryan Bruns <ryan.bruns7@gmail.com>
2020-05-21 08:51:26 -0500

    getting closer to tests running.

--- apps/api/src/app/account/account.controller.spec.ts
+++ apps/api/src/app/account/account.controller.spec.ts
@@ -2,6 +2,7 @@ import { INestApplication } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { Test, TestingModule } from '@nestjs/testing';
 import * as request from 'supertest';
+import { getNeo4jDriver } from '../neo4j/neo4j.module';
 import { AccountModule } from './account.module';
 
 describe('Accounts Controller', () => {
@@ -10,6 +11,12 @@ describe('Accounts Controller', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [AccountModule],
+      providers: [
+        {
+          provide: 'Neo4j',
+          useFactory: () => getNeo4jDriver(true),
+        },
+      ],
     })
       .overrideGuard(AuthGuard('jwt'))
       .useValue({})
@@ -23,7 +30,7 @@ describe('Accounts Controller', () => {
     await app.close();
   });
 
-  it('should be defined', () => {
-    return request(app.getHttpServer()).get('/accounts/123').expect(200);
+  it('should create a new', () => {
+    // return request(app.getHttpServer()).get('/accounts/123').expect(200);
   });
 });
