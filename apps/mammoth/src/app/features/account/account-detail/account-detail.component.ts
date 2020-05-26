4656aa85610ace121c0abae0b9ad7b4109413cce
Ryan Bruns <ryan.bruns7@gmail.com>
2020-05-26 09:00:20 -0500

    need some formatted values to make displaying transactions useful.

--- apps/mammoth/src/app/features/account/account-detail/account-detail.component.ts
+++ apps/mammoth/src/app/features/account/account-detail/account-detail.component.ts
@@ -3,13 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'mammoth-account-detail',
   templateUrl: './account-detail.component.html',
-  styleUrls: ['./account-detail.component.scss']
+  styleUrls: ['./account-detail.component.scss'],
 })
 export class AccountDetailComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  public onClickGetTransaction(): void {
+    // going to fire a query for testing. In order to nicely display the transactions in a grid. It needs to be
+    // a FormattedValue of sorts.
   }
-
 }
