a1d43d0f627ab5e1c02271f94be29835d4f2df83
Ryan Bruns <ryan.bruns7@gmail.com>
2020-06-05 08:36:05 -0500

    tailwind works, time for some storybook

--- apps/mammoth/src/app/landing/landing.component.ts
+++ apps/mammoth/src/app/landing/landing.component.ts
@@ -1,41 +1,8 @@
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
-
-interface ICardDetails {
-  title: string;
-  cols: number;
-  rows: number;
-}
 
 @Component({
   selector: 'mammoth-landing',
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.scss'],
 })
-export class LandingComponent {
-  public cards: Observable<ICardDetails[]>;
-
-  constructor(private breakpointObserver: BreakpointObserver) {
-    this.cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
-      map(({ matches }) => {
-        if (matches) {
-          return [
-            { title: 'Card 1', cols: 1, rows: 1 },
-            { title: 'Card 2', cols: 1, rows: 1 },
-            { title: 'Card 3', cols: 1, rows: 1 },
-            { title: 'Card 4', cols: 1, rows: 1 },
-          ];
-        }
-
-        return [
-          { title: 'Card 1', cols: 2, rows: 1 },
-          { title: 'Card 2', cols: 1, rows: 1 },
-          { title: 'Card 3', cols: 1, rows: 2 },
-          { title: 'Card 4', cols: 1, rows: 1 },
-        ];
-      })
-    );
-  }
-}
+export class LandingComponent {}
