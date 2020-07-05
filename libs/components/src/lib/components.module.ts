20bc06f7da18c2326b8b926638756cf44ab82601
Ryan Bruns <ryan.bruns7@gmail.com>
2020-07-05 17:09:58 -0400

    dialog seems pretty useless considering how it's created

--- libs/components/src/lib/components.module.ts
+++ libs/components/src/lib/components.module.ts
@@ -2,7 +2,6 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MammothBudgetComponentModule } from './budget-tile/budget-component.module';
 import { MammothButtonModule } from './button/button.module';
-import { MammothDialogModule } from './dialog/dialog.module';
 
 @NgModule({
   imports: [CommonModule, MammothButtonModule, MammothBudgetComponentModule, MammothDialogModule],
