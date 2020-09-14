15da7e7069eec62058846ea36b09a0a8fea062d9
Ryan Bruns <ryan.bruns7@gmail.com>
2020-09-14 08:13:44 -0500

    add a title

--- apps/insights/src/app/protected/views/UpcomingExpenseView.tsx
+++ apps/insights/src/app/protected/views/UpcomingExpenseView.tsx
@@ -1,9 +1,11 @@
+import { Typography } from '@material-ui/core'
 import React from 'react'
 import { FutureTransactionTableView } from '../components'
 
 export const UpcomingExpenseView = (): JSX.Element => {
   return (
     <section>
+      <Typography variant="h4">Upcoming Expenses</Typography>
       <FutureTransactionTableView />
     </section>
   )
