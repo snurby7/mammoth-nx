db2dc6906a7d1ef9c53d7d7946c84e9cb4cdefae
Ryan Bruns <ryan.bruns7@gmail.com>
2020-09-21 08:10:12 -0500

    starting to build out the categories

--- apps/insights/src/app/protected/pages/CategoryBreakdownPage.tsx
+++ apps/insights/src/app/protected/pages/CategoryBreakdownPage.tsx
@@ -1,5 +1,31 @@
-import React from 'react'
+import { CustomTreeData, TreeDataState } from '@devexpress/dx-react-grid'
+import { Grid, Table, TableHeaderRow, TableTreeColumn } from '@devexpress/dx-react-grid-material-ui'
+import { Paper } from '@material-ui/core'
+import React, { useState } from 'react'
+
+const getChildRows = (row, rootRows) => (row ? row.items : rootRows)
 
 export const CategoryBreakdownPage = () => {
-  return <div>Cmoing soon</div>
+  // TODO Feed this actual data for categories to consume and display.
+
+  const [columns] = useState([
+    { name: 'name', title: 'Name' },
+    { name: 'gender', title: 'Gender' },
+    { name: 'city', title: 'City' },
+    { name: 'car', title: 'Car' },
+  ])
+
+  const [tableColumnExtensions] = useState([{ columnName: 'name', width: 300 }])
+
+  return (
+    <Paper>
+      <Grid rows={[]} columns={columns}>
+        <TreeDataState />
+        <CustomTreeData getChildRows={getChildRows} />
+        <Table columnExtensions={tableColumnExtensions} />
+        <TableHeaderRow />
+        <TableTreeColumn for="name" />
+      </Grid>
+    </Paper>
+  )
 }
